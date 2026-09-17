0f0ae8828eed4b38aff17999c59e46aa0439bde2
pjheink <pjheink@users.noreply.github.com>
2026-09-17 15:26:50 -0400

    Update checkDormAvail.sh

--- checkDormAvail.sh
+++ checkDormAvail.sh
@@ -1,6 +1,6 @@
 # script to check the UW at Oshkosh dorm availability
-echo What's in the directory?
+echo "What is in the directory?"
 ls -a
 echo
-echo what are the environment variables?
+echo "What are the environment variables?"
 env
