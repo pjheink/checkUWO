183fcf893ccf527abf9b185ed8899040c01e20b7
pjheink <pjheink@users.noreply.github.com>
2026-09-17 15:45:07 -0400

    Update checkDormAvail.sh

--- checkDormAvail.sh
+++ checkDormAvail.sh
@@ -1,6 +1,10 @@
 # script to check the UW at Oshkosh dorm availability
-echo "What is in the directory?"
-ls -a
-echo
-echo "What are the environment variables?"
-env
+
+# define the URL
+URL="https://uwosh.starrezhousing.com/StarRezPortalXConference/F5839110/30/377/AirVenture_2026_Indi-Welcome_to_AirVentur?HadEmptyContext=True"
+
+# Fetch the page
+html=$(curl -sL "$URL")
+
+echo "=== find the matching phrase ==="
+echo "$html" | grep -o 'Check back later'
