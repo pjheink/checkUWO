3bd45f8473901a2fff216d63650280e372960603
pjheink <pjheink@users.noreply.github.com>
2026-09-18 10:44:13 -0400

    Update checkDormAvail.sh

--- checkDormAvail.sh
+++ checkDormAvail.sh
@@ -1,10 +1,11 @@
 # script to check the UW at Oshkosh dorm availability
 
 # define the URL
-URL="https://uwosh.starrezhousing.com/StarRezPortalXConference/F5839110/30/377/AirVenture_2026_Indi-Welcome_to_AirVentur?HadEmptyContext=True"
+# URL="https://uwosh.starrezhousing.com/StarRezPortalXConference/F5839110/30/377/AirVenture_2026_Indi-Welcome_to_AirVentur?HadEmptyContext=True"
+URL="https://uwosh.starrezhousing.com/StarRezPortalXConference/C410EE86/30/377/AirVenture_2026_Indi-Welcome_to_AirVentur?UrlToken=375C5A55"
 
 # Fetch the page
-html=$(curl -L "$URL")
+html=$(curl -sL "$URL")
 
 # find the matching phrase
 aaa="$html" | grep -o 'Check back later'
