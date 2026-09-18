04d5373825476eb370d09793c377fbabf0b08d23
pjheink <pjheink@users.noreply.github.com>
2026-09-18 10:20:04 -0400

    Update checkDormAvail.sh

--- checkDormAvail.sh
+++ checkDormAvail.sh
@@ -5,7 +5,7 @@ URL="https://uwosh.starrezhousing.com/StarRezPortalXConference/F5839110/30/377/A
 
 # Fetch the page
 #html=$(curl -sL "$URL")
-html=$(wget -q -0 - "$URL")
+html=$(wget -q - "$URL")
 
 # find the matching phrase
 aaa="$html" | grep -o 'Check back later'
